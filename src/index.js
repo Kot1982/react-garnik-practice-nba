17d6c40d1f7474fe6e83910dd5eda4fde1c6e72e
Kostiantyn Oliinyk <94046694+Kot1982@users.noreply.github.com>
2022-08-21 11:58:46 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/react-garnik-practice-nba/'>
+    <BrowserRouter basename='/kot1982.github.io/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
